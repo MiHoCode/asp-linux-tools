532b834e34a52eadfbf97b8ccee1abfd4cddb2c1
Michael H. <96173037+MiHoCode@users.noreply.github.com>
2026-02-17 09:42:46 +0100

    Update install_asptools.sh

--- scripts/install_asptools.sh
+++ scripts/install_asptools.sh
@@ -2,21 +2,19 @@
 set -eu
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "please execute as root (sudo)."
+  echo "please run as root (sudo)"
   exit 1
 fi
 
 SRC="https://raw.githubusercontent.com/MiHoCode/asp-linux-tools/main/scripts/"
 
-SCRIPT1="asp_create_service.sh"
-SCRIPT2="asp_create_webserver.sh"
+# Hier einfach neue Skripte ergänzen:
+SCRIPTS="asp_create_service.sh asp_create_webserver.sh"
 
-SRC1="${SRC}${SCRIPT1}"
-SRC2="${SRC}${SCRIPT2}"
-
-wget -O /usr/local/bin/${SCRIPT1} $SRC1
-chmod +x /usr/local/bin/${SCRIPT1}
-wget -O /usr/local/bin/${SCRIPT2} $SRC2
-chmod +x /usr/local/bin/${SCRIPT2}
+for SCRIPT in $SCRIPTS; do
+  echo "Installing $SCRIPT ..."
+  wget -O "/usr/local/bin/$SCRIPT" "${SRC}${SCRIPT}"
+  chmod +x "/usr/local/bin/$SCRIPT"
+done
 
 echo "Installed all tool scripts"
